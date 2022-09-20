3ab706bc2a3737adc24175c6b39e7649bac001cd
shaialoni <alonishai@gmail.com>
2022-09-20 15:17:11 -0700

    refactored code

--- findOutliers.js
+++ findOutliers.js
@@ -1,10 +1,19 @@
 function findOutlier(integers){
     //your code here
-    const odd = integers.filter(item => item % 2 !== 0)
-    const even = integers.filter(item => item % 2 === 0)
-    if (odd.length > even.length) {
-      return even[0]
-    } else return odd[0]
+    // const odd = integers.filter(item => item % 2 !== 0)
+    // const even = integers.filter(item => item % 2 === 0)
+    // if (odd.length > even.length) {
+    //   return even[0]
+    // } else return odd[0]
+
+    const even = []
+    const odd = []
+
+    integers.forEach(element => element % 2 !== 0 ? odd.push(element) : even.push(element))
+    
+    return odd.length > 1 ? even[0] : odd[0]
+    
+   
       
 }
 
